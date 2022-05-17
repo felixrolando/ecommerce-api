dd3d2dfa6b7d04bb95183b59290e96fac287ad86
Felix Rodriguez <rodriguezfelix17@gmail.com>
2022-05-16 21:38:09 -0400

    add jest and supertest

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application } from 'express'
+import routersV1 from './routes/Api/V1/index'
+const app: Application = express()
+
+app.use(express.json())
+
+app.get('/hello', (req, res) => {
+  res.send('hello world')
+})
+
+app.use('/api/admin/v1/', routersV1)
+
+export default app
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express from 'express'
-import routersV1 from './routes/Api/V1/index'
-const app = express()
+import app from './app'
 
-app.use(express.json())
-const PORT = 3000
-
-app.get('/ping', (req, res) => {
-  console.log('hola')
-  res.send('asdsad')
-})
-
-app.use('/api/admin/v1/', routersV1)
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log('server listen')
