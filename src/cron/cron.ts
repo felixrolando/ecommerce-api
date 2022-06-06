981eb05a5f0ed12051396f4e8f131dcae1b77660
Felix Rodriguez <= rodriguezfelix17@gmail.com>
2022-06-06 15:17:57 -0400

    add changes

--- src/cron/cron.ts
+++ src/cron/cron.ts
@@ -4,5 +4,10 @@ import { EVERY_30_SECONDS } from './scheduleConstants';
 export default () => {
     cron.schedule(EVERY_30_SECONDS, function () {
         console.log('running a task EVERY_30_SECONDS');
-    })
+    }
+        // , {
+        //     scheduled: true,
+        //     timezone: 'America/Santo_Domingo'
+        // }
+    )
 }
--- src/cron/scheduleConstants.ts
+++ src/cron/scheduleConstants.ts
@@ -4,3 +4,4 @@ export const EVERY_30_SECONDS = "*/30 * * * * *";
 export const EVERY_MINUTE = "* * * * * ";
 export const EVERY_30_MINUTES = "*/30 * * * *";
 export const EVERY_HOUR = "0 0 * * * *";
+export const EVERY_DAY = "5 0 * * *"; // At 12:05 AM
